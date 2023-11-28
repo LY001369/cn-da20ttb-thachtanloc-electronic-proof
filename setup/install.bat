3f6eca57e2d6afef5b994ec399821f1c78a0972f
LY001369 <LY001369@gmail.com>
2023-11-28 09:00:50 +0700

    Viết lại chức năng nhận diện bẳng

--- setup/install.bat
+++
@@ -1,5 +0,0 @@
-pip install opencv-python==4.8.1.78
-pip install patch_ng==1.17.4
-pip install vietocr==0.3.12
-pip3 install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu118
-pip3 install torchvision --index-url https://download.pytorch.org/whl/cu118
